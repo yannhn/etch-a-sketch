3dbcf303667afdf8bda40ea549afae74ac8a5f91
Yannik Hangst <yanso1@posteo.de>
2022-07-25 17:39:20 +0200

    setup grid

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const createGrid = () => {};
-
 const testDiv = document.createElement("div");
 testDiv.innerHTML = "<h2>This is created using js</h2>";
 document.body.appendChild(testDiv);
+
+const gridContainer = document.querySelector(".container");
+gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
+gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
+
+for (let i = 0; i < 256; i++) {
+  let singleDiv = document.createElement("div");
+  singleDiv.style.backgroundColor = "blue";
+  gridContainer.appendChild(singleDiv);
+}
