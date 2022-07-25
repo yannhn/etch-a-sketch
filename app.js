af98eaade56d69384a682d121e9e1c23da56dcdc
Yannik Hangst <yanso1@posteo.de>
2022-07-25 19:27:59 +0200

    add user input

--- app.js
+++ app.js
@@ -2,20 +2,34 @@ const testDiv = document.createElement("div");
 testDiv.innerHTML = "<h2>This is created using js</h2>";
 document.body.appendChild(testDiv);
 
-const gridContainer = document.querySelector(`[data-js="container"]`);
-gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
-gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
+const createGrid = (gridSize) => {
+  const gridContainer = document.querySelector(`[data-js="container"]`);
+  gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-  let singleDiv = document.createElement("div");
-  singleDiv.style.backgroundColor = "crimson";
-  gridContainer.appendChild(singleDiv);
-  singleDiv.addEventListener("mouseover", () => {
-    singleDiv.style.backgroundColor = "blue";
-  });
-}
+  const gridItems = gridContainer.querySelectorAll("div");
+  gridItems.forEach((gridItem) => gridItem.remove());
+
+  const gridAmount = gridSize * gridSize;
+
+  for (let i = 0; i < gridAmount; i++) {
+    let singleDiv = document.createElement("div");
+    singleDiv.style.backgroundColor = "crimson";
+    gridContainer.appendChild(singleDiv);
+
+    singleDiv.addEventListener("mouseover", () => {
+      singleDiv.style.backgroundColor = "blue";
+    });
+  }
+};
+
+createGrid(16);
 
 const changeSizeButton = document.querySelector('[data-js="changeSizeButton"]');
 changeSizeButton.addEventListener("click", () => {
-  alert("TEST");
+  const askUserInput = prompt("Enter grid size");
+  const parseUserInput = parseInt(askUserInput);
+  parseUserInput <= 100
+    ? createGrid(parseUserInput)
+    : alert("your number is too big");
 });
