b7baf2d663cd8ea45431c26556385d3b2179205f
Yannik Hangst <yanso1@posteo.de>
2022-07-25 17:54:48 +0200

    add button

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const testDiv = document.createElement("div");
 testDiv.innerHTML = "<h2>This is created using js</h2>";
 document.body.appendChild(testDiv);
 
-const gridContainer = document.querySelector(".container");
+const gridContainer = document.querySelector(`[data-js="container"]`);
 gridContainer.style.gridTemplateRows = "repeat(16, 1fr)";
 gridContainer.style.gridTemplateColumns = "repeat(16, 1fr)";
 
 for (let i = 0; i < 256; i++) {
   let singleDiv = document.createElement("div");
-  singleDiv.style.backgroundColor = "blue";
+  singleDiv.style.backgroundColor = "crimson";
   gridContainer.appendChild(singleDiv);
 }
+
+const changeSizeButton = document.querySelector('[data-js="changeSizeButton"]');
+changeSizeButton.addEventListener("click", () => {
+  alert("TEST");
+});
